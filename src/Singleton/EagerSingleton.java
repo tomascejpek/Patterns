da5acf35d4e9eb9b6f0f94c3e6699d20b6628823
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-02-20 18:42:55 +0100

    update class name

---
+++ src/Singleton/EagerSingleton.java
@@ -0,0 +1,17 @@
+package Singleton;
+
+/**
+ * Created by Tomas Cejpek on 18/02/2018.
+ */
+public class EagerSingleton {
+	private static final EagerSingleton instance = new EagerSingleton();
+
+	private EagerSingleton() {
+	}
+
+	public static EagerSingleton getInstance() {
+		return instance;
+	}
+
+}
+
