bdc74cbc073e4bd8967859b195dc34edba9b6132
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-02-22 22:24:58 +0100

    add builder test

--- src/main/GoF/builder/Car.java
+++ src/main/GoF/builder/Car.java
@@ -7,8 +7,16 @@ public class Car {
 	private int wheels;
 	private String color;
 
+	public String getColor() {
+		return color;
+	}
+
 	public void setColor(String color) {
 		this.color = color;
+	}
+
+	public int getWheels() {
+		return wheels;
 	}
 
 	public void setWheels(int wheels) {
---
+++ src/test/GoF/builder/builderTest.java
@@ -0,0 +1,31 @@
+package test.GoF.builder;
+
+import main.GoF.builder.BlueCarBuilder;
+import main.GoF.builder.Car;
+import main.GoF.builder.Director;
+import main.GoF.builder.GreenCarBuilder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Tomas Cejpek on 22/02/2018.
+ */
+public class builderTest {
+
+	private static final Director director = new Director();
+
+	@Test
+	public void greenCarBuilderTest() {
+		Car green = director.build(new GreenCarBuilder());
+		assertEquals(green.getWheels(), 4);
+		assertEquals(green.getColor(), "green");
+	}
+
+	@Test
+	public void blueCarBuilderTest() {
+		Car blue = director.build(new BlueCarBuilder());
+		assertEquals(blue.getWheels(), 3);
+		assertEquals(blue.getColor(), "blue");
+	}
+}
