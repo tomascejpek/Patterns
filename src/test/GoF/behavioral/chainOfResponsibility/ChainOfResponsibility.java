24e9e8e9876da32b2f7c4ea2a7995c4bc22c5ae3
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-02-27 20:30:30 +0100

    add tests

---
+++ src/test/GoF/behavioral/chainOfResponsibility/ChainOfResponsibility.java
@@ -0,0 +1,40 @@
+package test.GoF.behavioral.chainOfResponsibility;
+
+import main.GoF.behavioral.chainOfResponsibility.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+/**
+ * Created by Tomas Cejpek on 27/02/2018.
+ */
+public class ChainOfResponsibility {
+
+	private final Handler handler = new Handler();
+	private final Request request = new Request(0);
+
+	@Test
+	public void managerTest() {
+		request.setAmount(80);
+		assertSame(ManagerPP.class, handler.handle(request).getClass());
+	}
+
+	@Test
+	public void directorTest() {
+		request.setAmount(200);
+		assertSame(DirectorPP.class, handler.handle(request).getClass());
+	}
+
+	@Test
+	public void presidentTest() {
+		request.setAmount(600);
+		assertSame(PresidentPP.class, handler.handle(request).getClass());
+	}
+
+	@Test
+	public void toMuch() {
+		request.setAmount(2000);
+		assertNull(handler.handle(request));
+	}
+}
