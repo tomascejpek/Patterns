e6be13996603178685d94c5cb36e66d029cba1d8
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-03-03 15:53:49 +0100

    fix warnings

--- src/main/GoF/behavioral/iterator/Iterator.java
+++ src/main/GoF/behavioral/iterator/Iterator.java
@@ -3,7 +3,8 @@ package main.GoF.behavioral.iterator;
 /**
  * Created by Tomas Cejpek on 03/03/2018.
  */
-public interface Iterator<T> {
+@SuppressWarnings("unused")
+interface Iterator<T> {
 
 	boolean hashNext();
 
